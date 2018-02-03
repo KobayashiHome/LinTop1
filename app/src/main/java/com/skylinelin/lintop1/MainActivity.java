de70dc17b67b945434ac40b64e7746a9daa5135e
skylinelin <linfayang@live.com>
2018-02-03 22:17:13 +0800

    No2

--- app/src/main/java/com/skylinelin/lintop1/MainActivity.java
+++ app/src/main/java/com/skylinelin/lintop1/MainActivity.java
@@ -2,8 +2,11 @@ package com.skylinelin.lintop1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
